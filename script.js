d10bf47634c5ca099ea03a761c09c8fc90eec985
The5ive6ix <kbinyounus22@gmail.com>
2023-11-06 15:05:34 +0600

    vorteksmart

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
-const header = document.querySelector("header");
+// JS (script.js)
+const header = document.getElementById('main-header');
 
-window.addEventListener ("scroll",function() {
-    header.classList.toggle ("sticky", window.scrollY > 0);
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 0) {
+    header.classList.add('sticky');
+  } else {
+    header.classList.remove('sticky');
+  }
 });
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+  menu.classList.toggle('bx-x');
+  navlist.classList.toggle('open');
+};
+
+window.onscroll = () => {
+  menu.classList.remove('bx-x');
+  navlist.classList.remove('open');
+};
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
